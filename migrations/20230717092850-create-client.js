7b9ef7469149ac0431a22686b4bb6c6e29d81713
issuebombom <issuebombom@gmail.com>
2023-07-18 16:27:58 +0900

    chore: minor

--- migrations/20230717092850-create-client.js
+++ migrations/20230717092850-create-client.js
@@ -22,7 +22,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       phone_num: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
       },
       client_image: {
         type: Sequelize.STRING,
