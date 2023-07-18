7015541644d3ce750248c27a0ed992aa6505bafa
ug <dbwldms25@gmail.com>
2023-07-18 11:21:09 +0900

    upload

---
+++ routes/orders.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const OrdersController = require('../controllers/orders.controller');
+const ordersController = new OrdersController();
+
+// 주문하기 (고객)
+router.post('/order', ordersController.order);
+
+// 주문받기 (사장)
+router.get('/order-receive', (req, res) => {});
+
+module.exports = router;
